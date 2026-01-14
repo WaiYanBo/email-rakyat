dded4a0e1d0d7041f14cc87e833fcebed5bcf9c4
Wai Yan Bo <shoaiev126.wyb@gmail.com>
2026-01-14 17:52:42 +0800

    Update Optimization3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap'; // Import the sitemap package
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://e-rakyat.com', // REPLACE THIS with your actual domain name
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [sitemap()] // Add sitemap to the integrations list
 });
